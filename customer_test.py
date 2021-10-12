d70d9d1362d038544f36c6b380eab96e4ed2ee13
patkamon <patkamon.a@ku.th>
2021-10-12 21:09:13 +0700

    finish problems 4.

--- customer_test.py
+++ customer_test.py
@@ -17,22 +17,19 @@ class CustomerTest(unittest.TestCase):
 		self.new_movie = Movie("Mulan", Movie.NEW_RELEASE)
 		self.regular_movie = Movie("CitizenFour", Movie.REGULAR)
 		self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
-		
 
 	def test_billing(self):
 		"""Test billing is correct."""
-		rent_new_movie = Rental(self.new_movie,5)
-		rent_children_movie = Rental(self.childrens_movie, 4)
-		rent_regular_movie = Rental(self.regular_movie, 3)
+		self.assertEqual(15, Rental(self.new_movie, 5).rental_price())
+		self.assertEqual(3, Rental(self.childrens_movie, 4).rental_price())
+		self.assertEqual(3.5, Rental(self.regular_movie, 3).rental_price())
 
-		self.assertEqual(15,rent_new_movie.rental_price())
-		self.assertEqual(3, rent_children_movie.rental_price())
-		self.assertEqual(3.5, rent_regular_movie.rental_price())
+	def test_renter_point(self):
+		"""Test renter point work properly."""
+		self.assertEqual(5, Rental(self.new_movie, 5).renter_point())
+		self.assertEqual(1, Rental(self.regular_movie, 3).renter_point())
+		self.assertEqual(1, Rental(self.childrens_movie, 4).renter_point())
 
-		self.assertEqual(5, rent_new_movie.renter_point())
-		self.assertEqual(1, rent_regular_movie.renter_point())
-		self.assertEqual(1, rent_children_movie.renter_point())
-	
 	def test_statement(self):
 		stmt = self.c.statement()
 		# visual testing
